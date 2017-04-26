6382b1ecbbbf96fa0dd3e82574e1169477a9ac89
Renan Pupin <renanpupin@Renan-Mac.local>
2017-04-26 20:36:47 -0300

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Main from './scenes/main';
+
+export default class rnplayer extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+            <Main/>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  statusBarUnderlay: {
+    height: 24,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+  },
+});
+
+AppRegistry.registerComponent('rnplayer', () => rnplayer);
